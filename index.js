954d6dfe3cf4c966bb2957326bf0055f11c34cd5
DESKTOP-1FBD7EM\mingz <mingzheshi@126.com>
2021-01-19 23:57:39 +0800

    把之前写的联动和判断改成备选，现在看用不到

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ export { attachment } from "./attachment";
 export { warpAPI, warpModalSubmit } from "./business";
 export { accAdd, accSubtr, accMul } from "./calculation";
 export { compose } from "./compose";
-export { linkage, linkageRelationship } from "./linkage";
 export { recursive } from "./operation";
 export { warpState, onceState, strategy, baseState } from "./state";
-export { strategyRelationship } from "./strategyRelationship";
