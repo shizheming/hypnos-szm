f09ee8617801e3a33cc16411552fa0d3fda9331a
shape.shi <shi.zheming@towngas.com.cn>
2021-03-01 12:35:23 +0800

    删除relationship，未完成

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ export { warpAPI, warpModalSubmit } from "./business";
 export { accAdd, accSubtr, accMul } from "./calculation";
 export { compose } from "./compose";
 export { recursive } from "./operation";
-export { relation } from "./relationship";
 export { warpState, onceState, strategy, baseState } from "./state";
